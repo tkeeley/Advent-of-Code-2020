f110e189655aeb7c95645fe2a41149ab0770be4e
Tim Keeley <tkeeley@hotmail.com>
2020-12-02 08:08:13 -0500

    Day 2

---
+++ Day 2/Day 2.py
@@ -0,0 +1,33 @@
+# Advent of Code Day 2
+
+# Part 1
+passwords = [line.rstrip('\n') for line in open("passwords.txt")]
+x = 0
+
+for i in range(len(passwords)):
+    numbers = passwords[i].split()[0].split("-")
+    char = passwords[i].split()[1].strip(":")
+    pass_split = passwords[i].split()[2]
+    minNum = int(numbers[0])
+    maxNum = int(numbers[1])
+    if pass_split.count(char) >= minNum and pass_split.count(char) <= maxNum:
+        print(pass_split)
+        x += 1
+print("The are", x, "valid passwords")
+
+# Part 2
+passwords = [line.rstrip('\n') for line in open("passwords.txt")]
+x = 0
+
+for i in range(len(passwords)):
+    numbers = passwords[i].split()[0].split(
+        "-")  #get both numbers (first and second)
+    char = passwords[i].split()[1].strip(":")  #get letter
+    pass_split = passwords[i].split()[2]  #get password
+    firstNum = int(numbers[0]) - 1  #first number
+    secondNum = int(numbers[1]) - 1  #second number
+    if pass_split[firstNum] != pass_split[secondNum] and (
+            pass_split[firstNum] == char or pass_split[secondNum] == char):
+        x += 1
+
+print("The are", x, "valid passwords")
